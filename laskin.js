0becd003e82f8311684e46242ed23993c2cd0ebd
Jyri Lindroos <jyri.lindroos@keuda.fi>
2020-09-14 13:36:56 +0300

    lisätty AC:n toiminto

--- laskin.js
+++ laskin.js
@@ -12,3 +12,8 @@ function laske()
   var laske = document.getElementById('tulos').value;
   document.getElementById('tulos').innerHTML = eval(numero);
 }
+function tyhjenna()
+{
+  numero = '';
+  document.getElementById('tulos').innerHTML = numero;
+}
