2e982397fa81e77f74a568cf009d1d20fb7998b0
Jyri Lindroos <jyrilindroos@Jyrin-MacBook.local>
2020-09-15 20:49:26 +0300

    %-lasku kuntoon

--- laskin.js
+++ laskin.js
@@ -11,7 +11,17 @@ function lisaa(luku)
 
 function laske()
 {
-  document.getElementById('tulos').innerHTML = eval(numero);
+  var indeksi, jono;
+  if(numero.search("%") > 0)
+  {
+    jono = numero.split('%');
+    numero = jono[0] * jono[1] / 100
+    document.getElementById('tulos').innerHTML = numero;
+  }
+  else{
+    document.getElementById('tulos').innerHTML = eval(numero);
+    numero = eval(numero);
+  }
 }
 function tyhjenna()
 {
