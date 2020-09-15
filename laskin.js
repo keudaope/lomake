e609a12fd570a9431ebcb744161168f9ca27a006
Jyri Lindroos <jyrilindroos@Jyrin-MacBook.local>
2020-09-15 05:16:09 +0300

    Korjauksia kotoa

--- laskin.js
+++ laskin.js
@@ -1,6 +1,8 @@
 var numero = '';
 var tulos = 0;
-var merkki = '';
+var luku = '';
+var taulu = ['+','-','*','/','%'];
+var index = 0;
 function lisaa(luku)
 {
   numero+= luku;
@@ -9,11 +11,26 @@ function lisaa(luku)
 
 function laske()
 {
-  var laske = document.getElementById('tulos').value;
   document.getElementById('tulos').innerHTML = eval(numero);
 }
 function tyhjenna()
 {
   numero = '';
+  document.getElementById('tulos').innerHTML = numero;
+}
+
+function poistaViimeisin()
+{
+  for(var i = 0; i < numero.length; i++)
+  {
+    for(var j = 0; j < taulu.length; j++)
+    {
+      if(numero[i] == taulu[j])
+      {
+        index = i;
+      }
+    }
+  }
+  numero = numero.substr(0, index);
   document.getElementById('tulos').innerHTML = numero;
 }
